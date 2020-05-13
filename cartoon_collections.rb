6cfd4fc72ecdae87ec2e61e4bb816b6c3cfb5645
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-13 22:41:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,25 +8,25 @@ end
 
 def summon_captain_planet(array)
   nu_array = []
-  i = 0 
-  while i < array.length 
+  i = 0
+  while i < array.length
     nu_array << array[i].capitalize + "!"
-    i += 1 
-  end 
+    i += 1
+  end
   nu_array
 end
 
 summon_captain_planet(planeteer_calls)
 
 
-def long_planeteer_calls(array) 
-  i = 0 
+def long_planeteer_calls(array)
+  i = 0
   if  array.any? {|i| i.length > 4}
     return true
-  else 
+  else
     return false
   i = i + 1
-  end 
+  end
 end
 
 def find_the_cheese(array)
